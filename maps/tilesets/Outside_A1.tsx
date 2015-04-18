7f413f602ce5cd6735e4dec371b92da8d22b3b81
Paul Baker <paul.n.baker@utah.edu>
2015-04-17 22:28:04 -0600

    Adding plunge map and some tilesets

---
+++ maps/tilesets/Outside_A1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Outside_A1" tilewidth="16" tileheight="16">
+ <image source="../../../../.PlayOnLinux/wineprefix/Steam/drive_c/Program Files/Steam/steamapps/common/RPGVXAce/rtp/Graphics/Tilesets/Outside_A1.png" width="512" height="384"/>
+</tileset>
---
+++ maps/tilesets/Outside_B.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Outside_B" tilewidth="32" tileheight="32">
+ <image source="../../../../.PlayOnLinux/wineprefix/Steam/drive_c/Program Files/Steam/steamapps/common/RPGVXAce/rtp/Graphics/Tilesets/Outside_B.png" width="512" height="512"/>
+</tileset>
